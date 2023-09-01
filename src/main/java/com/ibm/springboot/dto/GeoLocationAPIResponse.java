a83372168cccfc3f15e469320b8fe6170335739c
Darshak Kachchhi <87572369+darshak-k@users.noreply.github.com>
2023-09-01 02:27:28 -0400

    DTO Geo API Location added

---
+++ src/main/java/com/ibm/springboot/dto/GeoLocationAPIResponse.java
@@ -0,0 +1,94 @@
+package com.ibm.springboot.dto;
+
+public class GeoLocationAPIResponse {
+	private String query;
+	private String status;
+	private String country;
+	private String countryCode;
+	private String regionName;
+	private String city;
+	private String zip;
+	private double lat;
+	private double lon;
+	private String timezone;
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public String getRegionName() {
+		return regionName;
+	}
+
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLon() {
+		return lon;
+	}
+
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+
+	public String getTimezone() {
+		return timezone;
+	}
+
+	public void setTimezone(String timezone) {
+		this.timezone = timezone;
+	}
+}
